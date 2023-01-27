c69758c7ec79ad962ac87d7eee230e6c51bd49ad
kevin-liuu <119897485+kevin-liuu@users.noreply.github.com>
2023-01-26 22:18:37 -0700

    created persons' class with properties

---
+++ personslab/personslab/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personslab
+{
+    internal class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColour;
+        private bool isWorking;
+
+        public int Id { get => id; set => id = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+        public string FavoriteColour { get => favoriteColour; set => favoriteColour = value; }
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+
+        public Person(int id, string firstName, string lastName, int age, string favoriteColour, bool isWorking)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.favoriteColour = favoriteColour;
+            this.isWorking = isWorking;
+            
+        }
+        public void DisplayPersonInfo() // Displays persons' full name, id and favorite color in a sentence 
+        {
+            Console.WriteLine(id + ":" + firstName + " " + lastName + "'s favorite color is " + favoriteColour);
+        }
+
+        public void ChangeFavoriteColor() // changes favorite color to white
+        {
+            this.favoriteColour = "White";
+        }
+        public void GetAgeInTenYear ()
+        {
+            Console.WriteLine(firstName + " " + lastName + "'s Age in 10 years is: " + (age + 10));
+        }
+
+        public override string ToString() //overriding tostring method to log on console
+        {
+            string formatted = "";
+
+            formatted += "PersonID: " + id + "\n";
+            formatted += "First Name: " + firstName + "\n";
+            formatted += "Last Name: " + lastName + "\n";
+            formatted += "Favorite Color: " + favoriteColour + "\n";
+            formatted += "Age: " + age + "\n";
+            formatted += "IsWorking: " + isWorking;
+            return formatted;
+        }
+    }
+}
---
+++ personslab/personslab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personslab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Ian", "Brooks", 30, "Red", true);
+            Person person2 = new Person(2, "Gina", "James", 18, "Green", false);
+            Person person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+            Person person4 = new Person(4, "Mary", "Beals", 28, "Yellow", true);
+
+            person2.DisplayPersonInfo();
+            Console.WriteLine(person3.ToString());
+            person1.ChangeFavoriteColor();
+            person1.DisplayPersonInfo();    
+            person4.GetAgeInTenYear();
+            
+        }
+    }
+}
